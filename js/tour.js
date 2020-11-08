0ab9118ade52dc414387f81aa21c13b830b3b606
Lê Thành Tiến <lethanhtien532@gmail.com>
2020-11-08 14:22:06 +0700

    tien

--- js/tour.js
+++ js/tour.js
@@ -1,13 +1,9 @@
 function getInfo() {
-  var firstName = document.getElementById("fname").value;
-  var lastName = document.getElementById("lname").value;;
   var date = document.getElementById("date").value;
-  var email = document.getElementById("email").value;
   var d = document.getElementById("numPeople");
   var person = d.options[d.selectedIndex].text;
   var e = document.getElementById("destination");
   var destination = e.options[e.selectedIndex].text;
-  var note = document.getElementById("note").value;
   const listTour = JSON.parse(localStorage.getItem('listTour'));
 
   const listTourChoice = listTour.filter(data => {
