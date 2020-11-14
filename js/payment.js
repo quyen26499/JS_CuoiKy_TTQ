7db3f3abb4ee4f2bb2f062bdd6bd02ecfb45e6c9
Tran Gia Quyen <giaquyen.tran@gmail.com>
2020-11-14 23:54:03 +0700

    fix Q

--- js/payment.js
+++ js/payment.js
@@ -1,35 +1,57 @@
- function onloadCompleted() {
-
-  var tour = JSON.parse(localStorage.getItem("tourBooking"));
-  var res1 = '';
-  var img = `
-    <img src="${tour[0].img}" alt="Card image cap" style="width: 80px; height: 80px;"> `;
-  var row = "<tr>";
-  row += "<td> " + img + "</td>";
-  row += `<td>${tour[0].name}</td>`;
-  row += `<td>${tour[0].datetour} </td>`;
-  row += `<td>${tour[0].datetour} </td>`;
-  row += `<td>${tour[0].price} VND </td>`;
-  row += "</tr>";
-  res1 += row;
-  document.getElementById("tab").innerHTML = res1;
+function onloadCompleted() {
+    var tour = JSON.parse(localStorage.getItem("tourBooking"));
+    var res1 = '';
+    var img = `
+        <img src="${tour[0].img}" alt="Card image cap" style="width: 80px; height: 80px;"> `;
+    var row = "<tr>";
+    row += "<td> " + img + "</td>";
+    row += `<td>${tour[0].name}</td>`;
+    row += `<td>${tour[0].date} </td>`;
+    row += `<td>${tour[0].price} </td>`;
+    row += `<td>${tour[0].price} VND </td>`;
+    row += "</tr>";
+    res1 += row;
+    document.getElementById("tab").innerHTML = res1;
 }
 function payment() {
-  if (localStorage.getItem('tourBookingSuccess') != null) {
-    var tour = JSON.parse(localStorage.getItem('tourBooking'));
-    const data = JSON.parse(localStorage.getItem('tourBookingSuccess'));
-    data.push(tour);
-    localStorage.setItem('tourBookingSuccess', JSON.stringify(data));
 
-    alert('Thanh toán thành công');
-    window.location.href = 'index.html';
-  } else {
-    var tour = JSON.parse(localStorage.getItem('tourBooking'));
-    localStorage.setItem('tourBookingSuccess', JSON.stringify(tour));
+    var customer = JSON.parse(localStorage.getItem("customer"));
+    var total = JSON.parse(localStorage.getItem("total"));
+    var tourSuccess = JSON.parse(localStorage.getItem("tourSuccess")) || [];
+    
+    var user = JSON.parse(localStorage.getItem("user"));
+    var listTourChoice = JSON.parse(localStorage.getItem("listTourChoice"));
 
-  
-    alert('Thanh toán thành công');
-    window.location.href = 'index.html';
-  }
-
+      for (i in user) {
+        if (user[i].id == customer[0].id) {
+            if (user.name == "" || user.contact == "" || user.address == "") {
+                alert("Cật nhập thông tin tài khoản! ")  
+            
+            } else {
+                
+                for (j = 0; j <= tourSuccess.length; j++) {
+                    tourComplete = {
+                      id: j,
+                      date: listTourChoice.dateGo,
+                      person: listTourChoice.person,
+                      destination: listTourChoice.destination,
+                      // total: total,
+                      idCustomer: user[i].id,
+                      email: user[i].email,
+                      name: user[i].name,
+                      contact: user[i].contact,
+                      address: user[i].address,
+                      status: "Đang chờ xử lý",
+                      isActive: false,
+                    };
+                }
+            }
+        }
+      }
+      tourSuccess.push(tourComplete);
+      localStorage.setItem("tourSuccess", JSON.stringify(tourSuccess));
+      localStorage.removeItem("listTourChoice");
+      localStorage.removeItem("total");
+      alert("Đặt  tourthành công! Chúng tôi sẽ sớm liên hệ với bạn, xin cảm ơn");
+      window.location.href = "index.html";
 }
--- js/tour.js
+++ js/tour.js
@@ -1,24 +1,42 @@
 function getInfo() {
 
   var date = document.getElementById("date").value;
-
-
   var d = document.getElementById("numPeople");
   var person = d.options[d.selectedIndex].text;
   var e = document.getElementById("destination");
   var destination = e.options[e.selectedIndex].text;
-  const listTour = JSON.parse(localStorage.getItem('listTour'));
 
+  const listTour = JSON.parse(localStorage.getItem('listTour'));
+  var customer = JSON.parse(localStorage.getItem("customer")) || [];
+  
+  if(customer.length == 0 ){
+    alert('Vui lòng đăng nhập');
+    window.location.href = 'index.html';
+  }
+  else if(date == ""){
+    alert("Vui lòng chọn ngày!")
+} 
+
+else {
+  
   const listTourChoice = listTour.filter(data => {
     if (data.name == destination ) {
       console.log(destination )
       return true;
-
+  
     }
     return false;
   })
   console.log(listTourChoice)
   if (listTourChoice.length) {
+          Choice = {
+            idcustomer: customer[0].id,
+            email: customer[0].email,
+            dateGo: date,
+            person: person,
+            destination: destination,
+          }
+          listTourChoice.push(Choice);
     localStorage.setItem('listTourChoice', JSON.stringify(listTourChoice));
      window.location.href = 'booking.html';
   } else {
@@ -26,44 +44,65 @@ function getInfo() {
   }
 }
 
-function load() {
-  tour = JSON.parse(localStorage.getItem('listTour'));
 }
-if (localStorage.getItem('listTour') != null) {
+//booking
+function load() {
+  tour = JSON.parse(localStorage.getItem('listTourChoice'));
+}
+if (localStorage.getItem('listTourChoice') != null) {
   load();
 }
 
-    function functionBooking(){
-        var listTour = "";
-        for ( i in tour) {
-          var data =JSON.parse(JSON.stringify(tour[i])) ;
-           listTour += `
-
-                <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
-                    <a href="#" class="unit-1 text-center">
-                    <img src="${tour[i].img}" alt="Image" class="img-fluid">
-                    <div class="unit-1-text">
-                      <strong class="text-primary mb-2 d-block">${tour[i].price}</strong>
-
-                        <h3 class="unit-1-heading">${tour[i].img}</h3>
-                    </div>
-                    </a>
-                </div>
+function functionBooking() {
+  var listTour = "";
+  for (i in tour) {
+    var data = JSON.parse(JSON.stringify(tour[i]));
+    listTour += `
+           <div class="col-md-4 col-sm-6 single">
+           <div class="product">
+           <a href="detail.html" class="text-center">
+               <div class="product-img">
+                   <img class="img-responsive" src="${tour[i].img}" style="height: 400px;width: 415px;">                            
+               </div>
+                       
+               <div class="product-text">                            
+                   <h6 class="product-text-title" style="color: black;">                      
+                       ${tour[i].name}
+                   </h6>
+                   <p class="product-text-price">
+                   <i class="fa fa-dollar" style="font-size:24px"></i> 
+                   Giá :                            
+                       ${tour[i].price} VND
+                                                
+                   </p>
+                   <p class="product-text-price"> 
+                   <i class="fa fa-calendar" style="font-size:24px"></i>
+                   Thời gian :                           
+                       ${tour[i].date} ngày
+                                                
+                   </p>
+                   <p class="product-text-price"> 
+                   <i class="fa fa-barcode" style="font-size:24px"></i>
+                   Mã tour :                              
+                   ${tour[i].note}
+                                            
+               </p>
+               </div>
+               </a>
                <div class="product-button">                                                         
                    <button onclick="order(${tour[i].id})" class="btn btn-primary py-2 px-4 text-white">
                        Đặt ngay
                    </button>
                </div>
-
+           </div>
+         
+       </div>
        `;
-          document.getElementById("showtour").innerHTML = listTour ;
-          
-    }
-    
-}
-function order() {
-    const tourBooking = tour.filter(data => data.id === id)
-    localStorage.setItem('tourBooking', JSON.stringify(tourBooking));
-    window.location.href = 'payment.html';
+    document.getElementById("showtour").innerHTML = listTour;
   }
-
+}
+function order(id) {
+  const tourBooking = tour.filter(data => data.id === id)
+  localStorage.setItem('tourBooking', JSON.stringify(tourBooking));
+  alert("Đã thêm Tour! Vui lòng kiểm tra Tour!")
+}
